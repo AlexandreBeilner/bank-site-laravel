4f735c33ebcdbd30b78115836f397aabe8dd547f
Alexandre Beilner <alexandrebeilner8@gmail.com>
2025-11-21 21:28:58 -0300

    feat: add database schema for customers, banks and billets

---
+++ database/migrations/2025_11_21_235818_create_billets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('billets', function (Blueprint $table) {
+            $table->id();
+            $table->string('payer_name');
+            $table->string('payer_document');
+            $table->string('recipient_name');
+            $table->string('recipient_document');
+            $table->decimal('amount', 15, 2);
+            $table->date('expiration_date');
+            $table->string('observations')->nullable();
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId('bank_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('billets');
+    }
+};
